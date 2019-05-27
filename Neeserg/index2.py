8ae82c720bdff7e2443535c0987aa0e3f304423a
neeserg parajuli <neeserg@student.unimelb.edu.au>
2019-05-27 15:17:49 +1000

    Training included files revision necessary

---
+++ Neeserg/index2.py
@@ -0,0 +1,62 @@
+##export XAPIAN_FLUSH_THRESHHOLD=200000; python index_alldoc.py
+import xapian
+import time
+DATA_FILEPATH = "/home/neeserg/Dropbox/Webtext/Data/wiki-pages-text/"
+DATABASE_FILEPATH = "/home/neeserg/Dropbox/Webtext/Data/XxapianDatabase2"
+start = time.time()
+db = xapian.WritableDatabase(DATABASE_FILEPATH, xapian.DB_CREATE_OR_OPEN)
+
+termgenerator = xapian.TermGenerator()
+termgenerator.set_stemmer(xapian.Stem("en"))
+text = ''
+content = ''
+prev_docid = ''
+j = 0
+first_done = False
+for i in range(1,110):
+    st = "wiki-{:03d}.txt".format(i)
+    cyclestart = time.time()
+    print(st)
+    with open(DATA_FILEPATH + st) as file:
+       
+        #Create the databse
+        for line in file:
+            words = line.split(' ')
+            
+            #extract the title from the id
+
+            id1 = words[0]
+            docId = u"Q"+id1 + "_~s~_"
+            id2 = words[1]
+            if not id2.isdigit():
+                continue
+            
+            
+            if(docId != prev_docid and first_done):
+                doc = xapian.Document()
+                termgenerator.set_document(doc)
+                termgenerator.index_text(content)
+                termgenerator.index_text(title, 1,'S')
+                doc.set_data(text)
+                doc.add_boolean_term(prev_docid)
+                db.replace_document(prev_docid, doc)
+                
+                if (j%10000 == 0):
+                    print(prev_docid +"--------" + text)
+                    print(content)
+                    print(j)
+                text = ''
+                content = ''
+                j+=1
+
+            ############# update document information
+            title = id1.split('_')
+            title = ' '.join(title)
+            content += ' '.join(words[2:])
+            text += '*_*' + id2 + ' '.join(words[2:])
+            prev_docid = docId
+            first_done = True
+
+    print(st, time.time() - cyclestart)
+
+print("This program took this many seconds: ",time.time() - start )
---
+++ Neeserg/index_alldoc.py
@@ -0,0 +1,58 @@
+##export XAPIAN_FLUSH_THRESHHOLD=200000; python index_alldoc.py
+import xapian
+import time
+DATA_FILEPATH = "/Users/neesergparajuli/Dropbox/Webtext/Data/wiki-pages-text/"
+DATABASE_FILEPATH = "/Users/neesergparajuli/Dropbox/Webtext/Data/XxapianDatabase"
+start = time.time()
+db = xapian.WritableDatabase(DATABASE_FILEPATH, xapian.DB_CREATE_OR_OPEN)
+
+termgenerator = xapian.TermGenerator()
+termgenerator.set_stemmer(xapian.Stem("en"))
+
+for i in range(1,110):
+    st = "wiki-{:03d}.txt".format(i)
+    cyclestart = time.time()
+    print(st)
+    j = 0
+    with open(DATA_FILEPATH + st) as file:
+       
+        #Create the databse
+        for line in file:
+            words = line.split(' ')
+            
+            #extract the title from the id
+
+            id1 = words[0]
+            title = id1.split('_')
+            title = ' '.join(title)
+            title = title.split('-')
+            title = ' '.join(title)
+
+            #check fact number is given and create doc ID
+            id2 = words[1]
+            if not id2.isdigit():
+                continue
+            docId = u"Q"+id1 + "_~s~_"+id2
+
+            #create main fact refference
+            text = ' '.join(words[2:])
+            doc = xapian.Document()
+            termgenerator.set_document(doc)
+            termgenerator.index_text(text)
+            termgenerator.index_text(title, 1,'S')
+            if (j%10000 == 0):
+                print(docId, text)
+
+            doc.set_data(text)
+            doc.add_boolean_term(docId)
+            db.replace_document(docId, doc)
+            j+=1
+    print(st, time.time() - cyclestart)
+
+print("This program took this many seconds: ",time.time() - start )
+
+
+
+
+
+        
